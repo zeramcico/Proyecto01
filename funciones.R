2b3d2dd01352350633cb0391eb9add220c7f9413
Teresa Zelaya <teresa.zelaya@unah.hn>
2021-11-21 23:35:41 -0600

    se creo archivo de funciones en R

---
+++ funciones.R
@@ -0,0 +1,9 @@
+#trabajando con funciones
+#function(){}#en () va lo que vamos a ingresar se un objeto, 2,3 en {}lo que hace la función
+
+suma<-function(x,y){
+  adicion <- x+y
+  resta<-x-y
+  print(adicion) #imprimir adicion
+  print (resta) #imprimir resta
+}
